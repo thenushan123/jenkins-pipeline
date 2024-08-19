69af4c51e2777819401240139d9b5a94b118ed95
thenushan <thenushan.sivanesan@gmail.com>
2024-08-19 09:10:49 +0100

    first commit

---
+++ src/test/java/com/lfacademy/microservices/currencyexchangeservice/cucumber/CucumberSpringContextConfiguration.java
@@ -0,0 +1,19 @@
+package com.lfacademy.microservices.currencyexchangeservice.cucumber;
+
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.lfacademy.microservices.currencyexchangeservice.CurrencyExchangeServiceApplicationH2;
+
+import cucumber.api.java.Before;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = CurrencyExchangeServiceApplicationH2.class, loader = SpringBootContextLoader.class)
+public class CucumberSpringContextConfiguration {
+
+  @Before
+  public void setUp() {
+  }
+}
